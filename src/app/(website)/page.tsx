2ffcc678e8ef3814e1dda6a5863286573ac92fcb
pierrerrrr <pierrestorniolo@gmail.com>
2025-07-20 21:58:19 -0300

    core: restyling the app website - working on the pricing section next

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -5,12 +5,17 @@ import FeaturesSection from "@/components/global/website-sections/features";
 import FooterSection from "@/components/global/website-sections/footer";
 import HeroSection from "@/components/global/website-sections/hero-section";
 import Pricing from "@/components/global/website-sections/pricing";
+import GradientHero from "@/components/mvpblocks/gradient-hero";
+import Header2 from "@/components/mvpblocks/header-2";
+import PricingSection from "@/components/mvpblocks/pricing-5";
 
 export default function Home() {
   return (
     <section className="min-h-screen text-foreground">
-      <HeroSection />
-      <Pricing />
+      <Header2 />
+      <GradientHero />
+      {/* <PricingSection /> */}
+      {/* <Pricing /> */}
       <FeaturesSection />
       <Cta />
       <FooterSection />
